1d51f490c3465d37916fcfb47b82660f9c50e8c2
German De Francesco <germandefrancesco@hotmail.com>
2023-10-27 12:03:23 -0300

    Reason now working

---
+++ Api/ApiKeyAuthenticationHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace Api;
+
+public class ApiKeyAuthenticationHandler : AuthenticationHandler<ApiKeyAuthenticationOptions>
+{
+    public ApiKeyAuthenticationHandler(
+        IOptionsMonitor<ApiKeyAuthenticationOptions> options,
+        ILoggerFactory logger,
+        UrlEncoder encoder,
+        ISystemClock clock) : base(options, logger, encoder, clock) { }
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        var claims = new[] { new Claim(ClaimTypes.Name, "test@eternet.cc") };
+        var identity = new ClaimsIdentity(claims, Scheme.Name);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, Scheme.Name);
+        var result = AuthenticateResult.Success(ticket);
+        return Task.FromResult(result);
+    }
+}
+
+public class ApiKeyAuthenticationOptions : AuthenticationSchemeOptions
+{
+}
--- Api/Program.cs
+++ Api/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication()
+    .AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>("ApiKey", options => { });
 builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy(Policies.IsAuthorized, policy => policy.Requirements.Add(new IsAuthorizedRequirement()));
@@ -18,6 +20,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
